aaae82b85f1db509d1a1e840b774fcc555bcedf0
xenon136 <xenon136@ya.ru>
2022-11-12 01:39:12 +0300

    new fix

--- api/add_category_act.php
+++ api/add_category_act.php
@@ -1,12 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php 
  $host='lucky.db.elephantsql.com';
  $user='wulgcjle';
  $password='tYwgRYgZoK9N3hdNkyU604UrbpEZ2OIl';
  $db_name='wulgcjle';
  $category = $_POST["category"]
+ echo $category
  $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
- $STH = $DBH->prepare("INSERT INTO caregory (name) VALUES (?)"); 
+ $STH = $DBH->prepare("INSERT INTO caregory (name) VALUES (:name)"); 
+ $STH->bindParam(':name', $name);
  $STH->execute($category);
  $url = "https://db-project-six.vercel.app/api/add_category.php"
  header('Location: '.$url);
    ?>
+</html>
