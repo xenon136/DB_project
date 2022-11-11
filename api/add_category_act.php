5f4e3e95c9c03277dc6ce31fff96a1ddb88d6a04
xenon136 <xenon136@ya.ru>
2022-11-12 01:59:18 +0300

    add public

--- api/add_category_act.php
+++ api/add_category_act.php
@@ -12,7 +12,7 @@
  $category = $_POST["category"]
  echo $category
  $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
- $STH = $DBH->prepare("INSERT INTO caregory (name) VALUES (:name)"); 
+ $STH = $DBH->prepare("INSERT INTO public.caregory (name) VALUES (:name)"); 
  $STH->bindParam(':name', $name);
  $STH->execute();
  $url = "https://db-project-six.vercel.app/api/add_category.php"
