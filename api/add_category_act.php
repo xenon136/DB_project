9ca84f81a85703542c3656245661514b2996b19a
xenon136 <xenon136@ya.ru>
2022-11-12 01:30:59 +0300

    fix

--- api/add_category_act.php
+++ api/add_category_act.php
@@ -5,7 +5,7 @@
  $db_name='wulgcjle';
  $category = $_POST["category"]
  $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
- $STH = $DBH->prepare("INSERT INTO category (name) VALUES (?)"); 
+ $STH = $DBH->prepare("INSERT INTO caregory (name) VALUES (?)"); 
  $STH->execute($category);
  $url = "https://db-project-six.vercel.app/api/add_category.php"
  header('Location: '.$url);
