7c9e6b124bcd41a5a8da4cb5d815c3a6e9abcfb7
xenon136 <xenon136@ya.ru>
2022-11-11 21:27:23 +0300

    one more

--- api/request.php
+++ api/request.php
@@ -14,7 +14,7 @@
  $db_name='wulgcjle';
  $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
  $query = 'SELECT * FROM country';
- $STH = $myPDO->query($query);
+ $STH = $DBH->query($query);
  $STH->setFetchMode(PDO::FETCH_ASSOC);  
  while($row = $STH->fetch()) {  
   echo $row['id'] . "\n";  
