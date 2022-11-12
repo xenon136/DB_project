3e969630e142667cba4b95c2796f1a5614ab70b0
xenon136 <xenon136@ya.ru>
2022-11-12 09:00:56 +0300

    fix

--- api/add_product_act.php
+++ api/add_product_act.php
@@ -4,7 +4,7 @@
  $password='tYwgRYgZoK9N3hdNkyU604UrbpEZ2OIl';
  $db_name='wulgcjle';
  $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
- $STH = $DBH->prepare("INSERT INTO product (name, seller_id, color_id, company_id, category_id, price) VALUES (:name)"); 
+ $STH = $DBH->prepare("INSERT INTO product (name, seller_id, color_id, company_id, category_id, price) VALUES (:name, :seller_id, :color_id, :company_id, :category_id, :price)"); 
  $STH->bindParam(':name', $_POST["name"]);
  $STH->bindParam(':seller_id', (int)$_POST["seller"]);
  $STH->bindParam(':color_id', (int)$_POST["color"]);
