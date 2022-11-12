4769b710c147c759a2874dd43b26be1615a1764a
xenon136 <xenon136@ya.ru>
2022-11-12 09:35:17 +0300

    fix prod act

--- api/add_product_act.php
+++ api/add_product_act.php
@@ -6,11 +6,11 @@
  $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
  $STH = $DBH->prepare("INSERT INTO product (name, seller_id, color_id, company_id, category_id, price) VALUES (:name, :seller_id, :color_id, :company_id, :category_id, :price)"); 
  $STH->bindParam(':name', $_POST["name"]);
- $STH->bindParam(':seller_id', (int)$_POST["seller"]);
- $STH->bindParam(':color_id', (int)$_POST["color"]);
- $STH->bindParam(':company_id', (int)$_POST["company"]);
- $STH->bindParam(':category_id', (int)$_POST["category"]);
- $STH->bindParam(':price', (int)$_POST["price"]);
+ $STH->bindValue(':seller_id', (int)$_POST["seller"], PDO::PARAM_INT);
+ $STH->bindValue(':color_id', (int)$_POST["color"], PDO::PARAM_INT);
+ $STH->bindValue(':company_id', (int)$_POST["company"], PDO::PARAM_INT);
+ $STH->bindValue(':category_id', (int)$_POST["category"], PDO::PARAM_INT);
+ $STH->bindValue(':price', (int)$_POST["price"], PDO::PARAM_INT);
  $STH->execute();
  header('Location: https://db-project-six.vercel.app/api/index.php');
 ?> 
