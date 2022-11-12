d44d785c3ebb2adefd18cbe01c7175c25f13f807
xenon136 <xenon136@ya.ru>
2022-11-12 08:22:05 +0300

    add more actions

---
+++ api/act/add_company_act.php
@@ -0,0 +1,12 @@
+<?php 
+ $host='lucky.db.elephantsql.com';
+ $user='wulgcjle';
+ $password='tYwgRYgZoK9N3hdNkyU604UrbpEZ2OIl';
+ $db_name='wulgcjle';
+ $name = $_POST["name"];
+ $DBH = new PDO("pgsql:host=$host;dbname=$db_name;user=$user;password=$password");
+ $STH = $DBH->prepare("INSERT INTO company (name) VALUES (:name)"); 
+ $STH->bindParam(':name', $name);
+ $STH->execute();
+ header('Location: https://db-project-six.vercel.app/api/index.php');
+?> 
